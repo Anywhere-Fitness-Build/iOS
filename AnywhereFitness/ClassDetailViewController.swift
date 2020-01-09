5cf521e171cce8b3492a8c87826cef41bbeb3db0
Skyler Suzuki <skylersuzuk1@gmail.com>
2020-01-08 16:01:38 -0800

    Edited class detailviewcontroller to have a picker for intensity level instead of a segmented control. Updated label to reflect this.

--- AnywhereFitness/ClassDetailViewController.swift
+++ AnywhereFitness/ClassDetailViewController.swift
@@ -8,10 +8,10 @@
 
 import UIKit
 
-enum IntensityLevel: Int {
-    case low
-    case medium
-    case high
+enum IntensityLevel: String {
+    case low = "Low"
+    case medium = "Medium"
+    case high = "High"
 }
 
 class ClassDetailViewController: UIViewController, ClassDateViewControllerDelegate {
@@ -23,21 +23,29 @@ class ClassDetailViewController: UIViewController, ClassDateViewControllerDelega
     @IBOutlet private weak var durationTextField: UITextField!
     @IBOutlet private weak var startTimeLabel: UILabel!
     @IBOutlet private weak var registeredUsersLabel: UILabel!
-    @IBOutlet private weak var intensitySegmentedControl: UISegmentedControl!
+    @IBOutlet private weak var intensityPicker: UIPickerView!
     @IBOutlet private weak var registerButton: UIButton!
+    @IBOutlet private weak var intensityLabel: UILabel!
 
     // MARK: - Properties
     var fitnessClass: FitnessClass?
 
     var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
-        formatter.dateFormat = "MMM d, yyyy"
+        formatter.dateFormat = "MMM d, h:mm a"
         formatter.timeZone = TimeZone.autoupdatingCurrent
         return formatter
     }
 
+    let intensityPickerList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        intensityPicker.dataSource = self
+        intensityPicker.delegate = self
+
+        updateViews()
         self.tabBarController?.tabBar.isHidden = true
     }
 
@@ -50,24 +58,49 @@ class ClassDetailViewController: UIViewController, ClassDateViewControllerDelega
             durationTextField.text = fitnessClass.duration
             startTimeLabel.text = fitnessClass.startTime
             registeredUsersLabel.text = "Regi"
-
-            intensitySegmentedControl.selectedSegmentIndex = intensity(intensity: fitnessClass.intensity).rawValue
+            intensityPicker.selectedRow(inComponent: Int(fitnessClass.intensity))
+            intensityLabelView(intensity: fitnessClass.intensity)
             // register button will change based on user
         }
+
+
     }
 
-    private func intensity(intensity: Double) -> IntensityLevel {
-        if intensity < 4 {
-            return IntensityLevel.low
-        } else if intensity > 3 && intensity < 7 {
-            return IntensityLevel.medium
+    private func intensityLabelView(intensity: Double) {
+        if intensity < 3 {
+            intensityLabel.text = IntensityLevel.low.rawValue
+            intensityLabel.textColor = UIColor.green
+        } else if intensity > 2 && intensity < 7 {
+            intensityLabel.text = IntensityLevel.medium.rawValue
+            intensityLabel.textColor = UIColor.orange
         } else {
-            return IntensityLevel.high
+            intensityLabel.text = IntensityLevel.high.rawValue
+            intensityLabel.textColor = UIColor.red
         }
     }
 
     @IBAction func registerButtonTapped() {
-        
+        guard let classNameString = classNameTextField.text,
+            !classNameString.isEmpty,
+            let classTypeString = classTypeTextField.text,
+            !classTypeString.isEmpty,
+            let locationString = locationTextField.text,
+            !locationString.isEmpty,
+            let durationString = durationTextField.text,
+            !durationString.isEmpty,
+            let startTimeString = startTimeLabel.text else { return }
+
+
+
+        let _ = FitnessClass(name: classNameString,
+                             classType: classTypeString,
+                             startTime: startTimeString,
+                             duration: durationString,
+                             intensity: Double(intensityPicker.selectedRow(inComponent: 1)),
+                             location: locationString)
+
+        self.navigationController?.popViewController(animated: true)
+
     }
 
     func saveDateButtonWasPressed(date: Date) {
@@ -85,7 +118,23 @@ class ClassDetailViewController: UIViewController, ClassDateViewControllerDelega
                 datePickerVC.date = Date()
             }
         }
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    }
+}
+
+extension ClassDetailViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return intensityPickerList.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        intensityLabelView(intensity: Double(row))
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return intensityPickerList[row]
     }
 }
